b6dbef63db2b7d51bf9ad1a5b48dfa6d461a0c78
Pranjal-Sahu21 <sahupranjal1619@gmail.com>
2025-12-08 13:00:46 +0530

    Resolved weather issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,73 @@
-import React, { useEffect, useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Products from './pages/Products'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Cart from './pages/Cart'
-import Navbar from './components/Navbar'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
+import axios from "axios";
 
 const App = () => {
-
   const [location, setLocation] = useState();
   const [openDropDown, setOpenDropDown] = useState(false);
 
-  const getLocation = async() => {
-    navigator.geolocation.getCurrentPosition( async pos => {
-      const {latitude, longitude} = pos.coords;
-      const url = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`;      
-      try {
-        const location = await axios.get(url);
-        const exactLocation = location?.data?.address;
-        setLocation(exactLocation);
-        setOpenDropDown(false);
-
-      } catch (error) {
-        console.error(error);        
-      }
+  const getLocation = async () => {
+    if (!navigator.geolocation) {
+      console.error("Geolocation is not supported by this browser.");
+      return;
     }
-    )
-  }
-  
+
+    navigator.geolocation.getCurrentPosition(
+      async (pos) => {
+        const { latitude, longitude } = pos.coords;
+
+        try {
+          const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY; // Make sure you add this to your .env file
+          const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+          const response = await axios.get(url);
+          const data = response.data;
+
+          if (data && data.name && data.sys) {
+            setLocation({
+              city: data.name,
+              country: data.sys.country,
+            });
+          }
+
+          setOpenDropDown(false);
+        } catch (error) {
+          console.error("Failed to fetch location from OpenWeather:", error);
+        }
+      },
+      (err) => {
+        console.error("Geolocation error:", err);
+      }
+    );
+  };
+
   useEffect(() => {
-    getLocation()
-  }, [])
-  
+    getLocation();
+  }, []);
 
   return (
     <BrowserRouter>
-    <Navbar location={location} getLocation={getLocation} openDropDown={openDropDown} setOpenDropDown={setOpenDropDown}/>
+      <Navbar
+        location={location}
+        getLocation={getLocation}
+        openDropDown={openDropDown}
+        setOpenDropDown={setOpenDropDown}
+      />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/products' element={<Products/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-        <Route path='/cart' element={<Cart/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
